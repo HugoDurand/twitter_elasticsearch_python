2d50e8ec0fe039eaa48adb6358b081c094438db2
MewenLr <mewen.lr@gmail.com>
2019-03-28 20:23:42 +0100

    Bar chart d3

--- d3/index.js
+++ d3/index.js
@@ -1,112 +1,91 @@
-// const d3 =require('d3v4');
-
 // set the dimensions and margins of the graph
-// var margin = {top: 20, right: 20, bottom: 30, left: 40},
-// width = 960 - margin.left - margin.right,
-// height = 500 - margin.top - margin.bottom,
-// listKey = [],
-// listValue= [],
-// listData = [];
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    dataList = []
+    i = 0;
 
-// // set the ranges
-// var x = d3.scaleBand()
-//           .range([0, width])
-//           .padding(0.1);
+let colors = [ "#11144c", "#3a9679", "#fabc60", "#dd6b4d" ,"#e16262" ];
 
-// var y = d3.scaleLinear()
-//           .range([height, 0]);
+// set the ranges
+var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+var y = d3.scaleLinear()
+          .range([height, 0]);
 
-// // append the svg object to the body of the page
-// // append a 'group' element to 'svg'
-// // moves the 'group' element to the top left margin
-// var svg = d3.select("body").append("svg")
-//           .attr("width", width + margin.left + margin.right)
-//           .attr("height", height + margin.top + margin.bottom)
-//           .append("g")
-//           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-// // get the data
-// d3.csv("output.csv", (error, data) => {
+d3.csv("output.csv", function(error, data) {
+  Object.entries(data[0]).forEach(entry => {
+    let el = {
+      festival: entry[0],
+      occurrence: parseInt(entry[1]),
+      color: colors[i]
+    };
+    dataList.push(el);
+    i++;
+  });
 
-//   data = data[0];
-//   console.log("data =>", data)
+  if (error) throw error;
 
-//   Object.keys(data).map((objectKey, index) => {
-//     listKey.push(objectKey);
-//     value = data[objectKey];
-//     listValue.push(value);
-//   });
-//   listKey.unshift('Festivals');
-//   listValue.unshift('Itération');
-//   listData.push(listKey, listValue);
-//   console.log("listData =>", listData);
+  x.domain(dataList.map(function(d) { return d.festival; }));
+  y.domain([0, d3.max(dataList, function(d) { return d.occurrence; })]);
+
+  svg.selectAll(".bar")
+      .data(dataList)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.festival); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.occurrence); })
+      .attr("height", function(d) { return height - y(d.occurrence); })
+      .attr("fill", function(d){ return d.color });
+
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .attr("class", "axisx")
+      .call(d3.axisBottom(x));
+
+  svg.append("g")
+      .attr("class", "axisy")
+      .call(d3.axisLeft(y));
+
+  svg.selectAll(".text")
+      .data(dataList)
+      .enter().append('text')
+      .attr("class","label")
+      .attr("dy", ".75em")
+      .attr('x', function(d) { return x(d.festival); })
+      .attr('y', function(d) { return y(d.occurrence + 15); })
+      .attr("transform", "translate(67,0)")
+      .text(function(d) { return Math.round(y(d.occurrence)); });
+
+      // .attr("x", (function(d) { return xScale(d.food) + xScale.rangeBand() / 2 ; }  ))
+      // .attr("y", function(d) { return yScale(d.quantity) + 1; })
+      // .attr("dy", ".75em")
+      // .text(function(d) { return d.quantity; });
+
+      // .attr('x', function(d) { return x(d.festival); })
+      // .attr('y', function(d) { return y(d.occurrence); })
+      // .attr("height", function(d) { return height - y(d.occurrence); })
+      // .attr('text-anchor', 'middle')
+      // .text(function(d) { return y(d.occurrence); });
 
 
-  var svgWidth = 500;
-  var svgHeight = 300;
 
-  var svg = d3.select('svg')
-      .attr("width", svgWidth)
-      .attr("height", svgHeight)
-      .attr("class", "bar-chart");
+  // svg.selectAll(".text")
+  //     .data(dataList)
+  //     .enter().append('text')
+  //     .attr('x', -(height / 2) - margin)
+  //     .attr('y', margin / 2.4)
+  //     .attr('transform", "translate(0," + height + ")')
+  //     .attr('text-anchor', 'middle')
+  //     .text('Love meter (%)')
 
-  var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-
-  var barPadding = 5;
-  var barWidth = (svgWidth / dataset.length);
-
-  var barChart = svg.selectAll("rect")
-      .data(dataset)
-      .enter()
-      .append("rect")
-      .attr("y", function(d) {
-          return svgHeight - d
-      })
-      .attr("height", function(d) {
-          return d;
-      })
-      .attr("width", barWidth - barPadding)
-      .attr("transform", function (d, i) {
-          var translate = [barWidth * i, 0];
-          return "translate("+ translate +")";
-      });
-
-  // listData.forEach(function(d) {
-  //   d.date = parseDate(d.date);
-  //   d.value = +d.value;
-  // });
-
-  // x.domain(data.map(function(d) { return d.date; }));
-  // y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  // if (error) throw error;
-
-  // // format the data
-  // data.forEach((d) => {
-  //   d.sales = +d.sales;
-  // });
-
-  // // Scale the range of the data in the domains
-  // x.domain(data.map((d) => { return d.salesperson; }));
-  // y.domain([0, d3.max(data, (d) => { return d.sales; })]);
-
-  // // append the rectangles for the bar chart
-  // svg.selectAll(".bar")
-  //     .data(data)
-  //     .enter().append("rect")
-  //     .attr("class", "bar")
-  //     .attr("x", (d) => { return x(d.salesperson); })
-  //     .attr("width", x.bandwidth())
-  //     .attr("y", (d) => { return y(d.sales); })
-  //     .attr("height", (d) => { return height - y(d.sales); });
-
-  // // add the x Axis
-  // svg.append("g")
-  //     .attr("transform", "translate(0," + height + ")")
-  //     .call(d3.axisBottom(x));
-
-  // // add the y Axis
-  // svg.append("g")
-  //     .call(d3.axisLeft(y));
-
-// });
+});
